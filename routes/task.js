847c532280ee768c8e2e1473f0f8f587ca044d22
DESKTOP-ATCEUSN\abc <deepyatash786@gmail.com>
2019-05-16 20:29:33 +0530

    u

--- routes/task.js
+++ routes/task.js
@@ -817,9 +817,9 @@ let slug;
                 PostID=result1[i].ID;
                 PostDate=dateFormat(result1[i].post_date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
                 PostTitle=result1[i].post_title;
-                Hourspent= result1[i].hours_spent ;
+                Hourspent= result1[i].hours_spent ;  
                 slug=result1[i].slug
-if(result1[i].fb_review==0){
+if(result1[i].fb_review==0){ 
   fbfinalstatus="open";
 }
 if(result1[i].fb_review==1){
@@ -1074,7 +1074,7 @@ taskfeedback:(req, res) =>   {
                       return res.status(200).json({ status :200, message:"Data saved successfully.",wpstatus:1 });
                      }
                      else {  
-                      let qry3 = "update `wp_term_relationships` set term_taxonomy_id='23' WHERE `object_id` = '" + postid + "' and term_taxonomy_id='22'";    
+                      let qry3 = "update `wp_term_relationships` set term_taxonomy_id='26' WHERE `object_id` = '" + postid + "' and term_taxonomy_id='22'";    
                       db.query(qry3, (er3, result3) => {              
                          if (er3) {
                            return res.status(500).json({ message: 'errr5', status :500, msg:er3,wpstatus:0 });
